383b8362dc3980b8701b3029f50342567c5c7bad
sathish-java-code <sathishmadhu@hotmail.com>
2025-05-08 14:33:34 +0530

    sample APi code added

--- src/main/java/com/example/springbootcicddemo/springbootcruddemo/SpringBootCrudDemoApplication.java
+++ src/main/java/com/example/springbootcicddemo/springbootcruddemo/SpringBootCrudDemoApplication.java
@@ -12,7 +12,7 @@ public class SpringBootCrudDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCrudDemoApplication.class, args);
 
-        System.out.println("Appliation created succssfully");
+        System.out.println("Pipeline Appliation created succssfully");
     }
 
 
