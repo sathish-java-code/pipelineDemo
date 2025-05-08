3a234afc163c45899a9e461c512da8bcf1c0099d
sathish-java-code <sathishmadhu@hotmail.com>
2025-05-08 11:20:09 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootcicddemo/springbootcruddemo/SpringBootCrudDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootcicddemo.springbootcruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudDemoApplication.class, args);
+
+        System.out.println("appliation cretad succssfully");
+    }
+
+
+}
