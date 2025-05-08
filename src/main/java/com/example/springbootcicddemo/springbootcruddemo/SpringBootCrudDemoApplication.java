928a3118122f5a5c346ad46b178ae13e061ad90b
sathish-java-code <sathishmadhu@hotmail.com>
2025-05-08 11:49:16 +0530

    sample APi code added

--- src/main/java/com/example/springbootcicddemo/springbootcruddemo/SpringBootCrudDemoApplication.java
+++ src/main/java/com/example/springbootcicddemo/springbootcruddemo/SpringBootCrudDemoApplication.java
@@ -2,15 +2,24 @@ package com.example.springbootcicddemo.springbootcruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootCrudDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCrudDemoApplication.class, args);
 
-        System.out.println("appliation cretad succssfully");
+        System.out.println("Appliation created succssfully");
     }
 
 
+
+     @GetMapping("/hi")
+    public String createhi(){
+    return "helloo india !";
+    }
 }
+
